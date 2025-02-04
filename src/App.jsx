16eceda13024765b3ce0065d733c8a8b71cf9372
itzfahadoooo <foyindolapo@gmail.com>
2025-02-04 18:44:05 +0100

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -87,12 +87,38 @@ const App = () => {
         className="test"
       ></motion.div>
 
-      <motion.div drag whileDrag={{ scale: 1.2, backgroundColor: "#f00" }} className="test"></motion.div>
+      <motion.div
+        drag
+        whileDrag={{ scale: 1.2, backgroundColor: "#f00" }}
+        className="test"
+      ></motion.div>
       <div className="test"></div>
       <div className="test"></div>
       <div className="test"></div>
       <div className="test"></div>
       <div className="test"></div>
+
+      <div className="relative mx-auto grid h-32 w-96 place-content-center">
+        <h1 className="relative z-0 text-3xl font-black uppercase">
+          Show me on scroll
+        </h1>
+        <motion.div
+          initial={{
+            opacity: 1,
+          }}
+          whileInView={{
+            opacity: 0,
+          }}
+          viewport={{
+            //   once: true,
+            //   margin: "-200px",
+            amount: "all",
+          }}
+          onViewportEnter={() => console.log("Enter!")}
+          onViewportLeave={() => console.log("Exit!")}
+          className="absolute bottom-0 left-0 right-0 top-0 z-10 bg-indigo-500"
+        />
+      </div>
     </div>
   );
 };
